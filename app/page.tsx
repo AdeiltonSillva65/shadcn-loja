3da539cfa414e8dea00983ee317f6636cffd9717
Adeilton <adeiltonsillva65@gmail.com>
2026-04-21 21:10:11 -0300

    corrigindo

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,14 @@ import { Header } from "@/components/Header";
 const Page = () => {
   return(
      <div className="min-h-screen flex  justify-center">
-        <div className="w-full max-w-4xl mx-auto ">
-      <Header/>
-      <div className="mx-3 ">
-        ...
+          <div className="w-full max-w-4xl mx-auto ">
+          <Header/>
+          <div className="mx-3 ">
+            ...
+          </div>
+          <Footer/>
       </div>
-      <Footer/>
     </div>
-     </div>
     
   )
 }
